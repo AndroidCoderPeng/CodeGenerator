fe8757ca0f0d77a97e0448fb337b0773d2f3eddf
pengxianhong <290677893@qq.com>
2024-10-18 09:00:45 +0800

    添加空白图

---
+++ CodeGenerator/Converters/ContentViewConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CodeGenerator.Converters
+{
+    public class ContentViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(value != null, nameof(value) + " != null");
+            return (int)value == 0 ? "Collapsed" : "Visible";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
