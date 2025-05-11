e52f5eac561b48e24c0a8abb1a88e1cd5fedccc8
pengxianhong <290677893@qq.com>
2025-05-11 13:15:14 +0800

    refactor(CodeGenerator): 优化启动窗口的倒计时逻辑
    - 重构了 StartupWindow 类中的计时器逻辑
    - 引入 TotalSteps 常量和 timerInterval 字段，提高代码可读性
    - 将计时器事件处理程序提取到独立的方法 OnTimerTick 中
    - 添加了 OnClosed 方法来清理计时器资源
    - 增加了控制台日志输出，便于调试

--- CodeGenerator/Views/StartupWindow.xaml.cs
+++ CodeGenerator/Views/StartupWindow.xaml.cs
@@ -6,32 +6,41 @@ namespace CodeGenerator.Views
 {
     public partial class StartupWindow : Window
     {
-        private readonly DispatcherTimer _timer = new DispatcherTimer
-        {
-            Interval = new TimeSpan(0, 0, 0,0,1)
-        };
-
-        private int _counterTime = 50;
+        private const int TotalSteps = 50;
+        private readonly TimeSpan _timerInterval = new TimeSpan(0, 0, 0, 0, 1);
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
+        private int _counterTime = TotalSteps;
 
         public StartupWindow()
         {
             InitializeComponent();
-
-            //倒计时显示大屏Logo
+            Console.WriteLine(@"Start StartupWindow");
+            _timer.Interval = _timerInterval;
+            _timer.Tick += OnTimerTick;
+            Console.WriteLine(@"Init LoadingProgress");
             _timer.Start();
-            _timer.Tick += delegate
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            if (LoadingProgress != null && _counterTime > 0)
             {
-                if (_counterTime > 0)
-                {
-                    LoadingProgress.Value++;
-                    _counterTime--;
-                }
-                else
-                {
-                    _timer.Stop();
-                    DialogResult = true;
-                }
-            };
+                LoadingProgress.Value++;
+                _counterTime--;
+            }
+            else
+            {
+                _timer.Stop();
+                DialogResult = true;
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            _timer.Stop();
+            _timer.Tick -= OnTimerTick;
+            Console.WriteLine(@"Close StartupWindow");
         }
     }
 }
