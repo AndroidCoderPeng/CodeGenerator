864c9e7cdf35531b1e4818300a0109fdd9d5087c
pengxianhong <290677893@qq.com>
2025-05-11 13:27:31 +0800

    feat(CodeGenerator): 添加空文件夹视图功能
    - 新增 ContentViewConverter 和 EmptyViewConverter 转换器
    - 在 MainWindow.xaml 中实现空文件夹视图和文件列表的切换
    - 添加空文件夹视图的 UI 元素，包括图片和提示文本
    - 优化文件列表的样式和布局

---
+++ CodeGenerator/Converters/EmptyViewConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CodeGenerator.Converters
+{
+    public class EmptyViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(value != null, nameof(value) + " != null");
+            return (int)value == 0 ? "Visible" : "Collapsed";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
