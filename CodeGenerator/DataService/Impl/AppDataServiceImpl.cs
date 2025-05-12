f3a47dbe892d996bcfd3a4dae62ce5a1756353fd
pengxianhong <290677893@qq.com>
2025-05-12 10:02:20 +0800

    feat(file-suffix): 优化文件后缀列表展示和删除功能- 将文件后缀名前添加星号，统一展示格式- 调整文件后缀列表项的展示样式，增加删除按钮 - 优化删除按钮的样式和布局，提高用户体验

--- CodeGenerator/DataService/Impl/AppDataServiceImpl.cs
+++ CodeGenerator/DataService/Impl/AppDataServiceImpl.cs
@@ -8,29 +8,29 @@ namespace CodeGenerator.DataService.Impl
         {
             return new List<string>
             {
-                ".py", // Python
-                ".cpp", "hpp", // C++
-                ".c", "h", // C
-                ".java", // Java
-                ".cs", // C#
-                ".js", // JavaScript
-                ".go", // Go
-                ".vb", // Visual Basic
-                ".sql", // SQL
-                ".vue", // Vue.js
-                ".html", //HTML
-                ".ts", // TypeScript
-                ".php", // PHP
-                ".rs", // Rust
-                ".swift", // Swift
-                ".kt", // Kotlin
-                ".scala", // Scala
-                ".rb", // Ruby
-                ".pl", // Perl
-                ".cobol", // COBOL
-                ".f90", // Fortran
-                ".sh", // Shell Script
-                ".m" // Object-c
+                "*.py", // Python
+                "*.cpp", "*.hpp", // C++
+                "*.c", "*.h", // C
+                "*.java", // Java
+                "*.cs", // C#
+                "*.js", // JavaScript
+                "*.go", // Go
+                "*.vb", // Visual Basic
+                "*.sql", // SQL
+                "*.vue", // Vue.js
+                "*.html", //HTML
+                "*.ts", // TypeScript
+                "*.php", // PHP
+                "*.rs", // Rust
+                "*.swift", // Swift
+                "*.kt", // Kotlin
+                "*.scala", // Scala
+                "*.rb", // Ruby
+                "*.pl", // Perl
+                "*.cobol", // COBOL
+                "*.f90", // Fortran
+                "*.sh", // Shell Script
+                "*.m" // Object-c
             };
         }
     }
